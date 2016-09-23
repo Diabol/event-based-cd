628d3810a0c2dd230f94252ca8fac0ff3d0809d3
Marcus Philip <marcus@diabol.se>
2016-09-23 19:57:37 +0200

    Dashing Eventstore: Added rest call and json parse. No ouput yet.

--- dashing/jobs/eventstore.rb
+++ dashing/jobs/eventstore.rb
@@ -1,6 +1,10 @@
-# :first_in sets how long it takes before the job is first run. In this case, it is run immediately
+require 'rest-client'
+require 'json'
 
 SCHEDULER.every '3s' do
+  url = 'http://52.211.202.77/streams/newstream'
+  response = RestClient.get(url)
+  json = JSON.parse(response)
   events = [ { label: 'marcus', value: 'hej' }, { label: 'robert', value: 'ho' } ]
   send_event('cd_list', { items: events })
 end
