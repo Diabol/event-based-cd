ba92e0fa50644d9496f021eae4e76079e1ac022e
Marcus Philip <marcus@diabol.se>
2016-09-24 11:11:50 +0200

    Dashing eventstore job done

--- dashing/jobs/eventstore.rb
+++ dashing/jobs/eventstore.rb
@@ -2,9 +2,22 @@ require 'rest-client'
 require 'json'
 
 SCHEDULER.every '3s' do
-  url = 'http://52.211.202.77/streams/newstream'
-  response = RestClient.get(url)
-  json = JSON.parse(response)
-  events = [ { label: 'marcus', value: 'hej' }, { label: 'robert', value: 'ho' } ]
+  url = 'http://52.211.202.77/streams/cd-events'
+  streamResponse = RestClient.get(url, headers={Accept: "application/json"})
+  streamJson = JSON.parse(streamResponse)
+  #print "streamJson: #{streamJson}\n"
+  entries = streamJson['entries']
+  #print "entries: #{entries}\n"
+  events = Array.new
+  entries.each { |entry|
+    id = entry['id'].sub(/:2113/, '')
+    #print "id: #{id}\n"
+    response = RestClient.get(id, headers={Accept: "application/json"})
+    #print "response #{id}: #{response}\n"
+    json = JSON.parse(response)
+    #print "json: #{id}: #{json}\n"
+    events.push( { label: json['event'], value: json['status'] + ' - ' + json['date'] } )
+  }
+  #print "#{events}\n"
   send_event('cd_list', { items: events })
 end
