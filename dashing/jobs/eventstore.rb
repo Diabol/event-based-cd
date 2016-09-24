f0de86c88accf82178469541263681e4b3a65346
Marcus Philip <marcus@diabol.se>
2016-09-24 12:42:21 +0200

    New stream name

--- dashing/jobs/eventstore.rb
+++ dashing/jobs/eventstore.rb
@@ -2,12 +2,16 @@ require 'rest-client'
 require 'json'
 
 SCHEDULER.every '3s' do
-  url = 'http://52.211.202.77/streams/cd-events'
+  url = 'http://52.211.202.77/streams/cd-events-ng'
   streamResponse = RestClient.get(url, headers={Accept: "application/json"})
   streamJson = JSON.parse(streamResponse)
   #print "streamJson: #{streamJson}\n"
   entries = streamJson['entries']
   #print "entries: #{entries}\n"
+  
+  # TODO
+  #lastest_rev = entries[0].json['source_revision'][0..6]
+  
   events = Array.new
   entries.each { |entry|
     id = entry['id'].sub(/:2113/, '')
@@ -25,6 +29,7 @@ SCHEDULER.every '3s' do
       label: json['event'] + ': ' + source + ' ' + rev + ' ' + image,
       value: json['status'] + ': ' + json['msg'] + ' ' + json['link'] + ' ' + json['date']
     } )
+    
   }
   #print "#{events}\n"
   send_event('cd_list', { items: events })
